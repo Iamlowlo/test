d446cfb0da34ba73aca32524501cfc4bfbc1e3e6
Carlos Lopez Perez <iamlowlo@gmail.com>
2016-02-29 23:52:45 +0100

    scan and unscan functionalities added

--- app/coffee/checkout.coffee
+++ app/coffee/checkout.coffee
@@ -1,1 +1,42 @@
-console.log "hey jude"
+class Checkout
+	constructor: (@pricingRules) ->
+	cart:{}
+	scan: (product) ->
+		if @cart[product]?
+			@cart[product].qty++ 
+		else
+			@cart[product] = {'qty':1}
+	unscan: (product) ->
+		@cart[product].qty-- if @cart[product]?
+	total: () ->
+		@cart
+co = new Checkout 'pricingRules'
+
+$ ->
+	$cart_totals = $('#cart_totals')
+	$('#available_product_list').on "click",".add_product", (e) ->
+		e.preventDefault()
+		$('#cart_totals').find '.product_list li:first-child'
+			.addClass 'hidden'
+		co.scan $(this).data 'product-name'
+		productName = $(this).data 'product-name'
+		productList = $('#cart_totals').find('#'+ productName)
+		if productList.length
+			productList.removeClass 'unvisible'
+				.find '.qty'
+				.text co.cart[productName].qty
+
+	$('#cart_totals').on "click",".remove_product", (e) ->
+		e.preventDefault()
+		productName = $(this).data 'product-name'
+		productList = $('#cart_totals').find('#'+ productName)
+		if productList.length
+			co.unscan productName
+		if co.cart[productName].qty < 1
+			productList.addClass 'unvisible'
+		else
+			productList.find '.qty'
+				.text co.cart[productName].qty
+			
+	$cart_totals.on "click",".check_totals", (e) ->
+		console.log(co.total())
--- build/scripts/checkout.js
+++ build/scripts/checkout.js
@@ -1,1 +1,67 @@
-console.log("hey jude");
+var Checkout, co;
+
+Checkout = (function() {
+  function Checkout(pricingRules) {
+    this.pricingRules = pricingRules;
+  }
+
+  Checkout.prototype.cart = {};
+
+  Checkout.prototype.scan = function(product) {
+    if (this.cart[product] != null) {
+      return this.cart[product].qty++;
+    } else {
+      return this.cart[product] = {
+        'qty': 1
+      };
+    }
+  };
+
+  Checkout.prototype.unscan = function(product) {
+    if (this.cart[product] != null) {
+      return this.cart[product].qty--;
+    }
+  };
+
+  Checkout.prototype.total = function() {
+    return this.cart;
+  };
+
+  return Checkout;
+
+})();
+
+co = new Checkout('pricingRules');
+
+$(function() {
+  var $cart_totals;
+  $cart_totals = $('#cart_totals');
+  $('#available_product_list').on("click", ".add_product", function(e) {
+    var productList, productName;
+    e.preventDefault();
+    $('#cart_totals').find('.product_list li:first-child').addClass('hidden');
+    co.scan($(this).data('product-name'));
+    productName = $(this).data('product-name');
+    productList = $('#cart_totals').find('#' + productName);
+    if (productList.length) {
+      return productList.removeClass('unvisible').find('.qty').text(co.cart[productName].qty);
+    }
+  });
+  $('#cart_totals').on("click", ".remove_product", function(e) {
+    var productList, productName;
+    e.preventDefault();
+    productName = $(this).data('product-name');
+    productList = $('#cart_totals').find('#' + productName);
+    if (productList.length) {
+      co.unscan(productName);
+    }
+    if (co.cart[productName].qty < 1) {
+      return productList.addClass('unvisible');
+    } else {
+      return productList.find('.qty').text(co.cart[productName].qty);
+    }
+  });
+  return $cart_totals.on("click", ".check_totals", function(e) {
+    return console.log(co.total());
+  });
+});
